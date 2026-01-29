99b5ca2cc7b11e8bb8e15c4447b81023f0c49043
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-01-29 09:57:42 +0200

    betwine

--- question.py
+++ question.py
@@ -1,6 +1,8 @@
 import os
 
-os.chdir(r"C:\Users\User\Desktop\Working_with_files\test")
-print(os.listdir())
-os.chdir("..")
-print(os.getcwd())
+#path = r"C:\Users\User\Desktop\Working_with_files"
+def print_fils(path):
+  path = r"C:\Users\User\Desktop\Working_with_files"
+  for item in os.listdir(path):
+    
+
