0b5bb490870af82bb1884e9e320ff2ad922fd395
Yhuda ben dayan <0527196317ybd@gmail.com>
2026-01-29 15:36:18 +0200

    question_4 is done

--- question.py
+++ question.py
@@ -1,8 +1,11 @@
 import os
 
-#path = r"C:\Users\User\Desktop\Working_with_files"
 def print_fils(path):
-  path = r"C:\Users\User\Desktop\Working_with_files"
   for item in os.listdir(path):
+    full_path = os.path.join(path, item)
+    if os.path.isfile(full_path):
+      print(full_path)
+    else:
+      print_fils(full_path)  
     
-
+print_fils(r"C:\Users\User\Desktop\Working_with_files")  
